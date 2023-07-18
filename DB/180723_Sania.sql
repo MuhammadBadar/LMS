03053cbc70cf7d7963cbea2576d1a8764e5a3623
Hira Doll <hiranaeem789z@gmail.com>
2023-07-18 11:21:17 -0700

    test

--- DB/180723_Sania.sql
+++ DB/180723_Sania.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `city`
 --
 
+
+
+
 DROP TABLE IF EXISTS `city`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
