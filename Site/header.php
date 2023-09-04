c1697fc5268c3cec3f9641dc26086543031fbc61
NomanHussain1 <nomannhussain@gmail.com>
2023-09-04 00:48:26 -0700

    done header changes

--- Site/header.php
+++ Site/header.php
@@ -1,11 +1,11 @@
-<head>
-  
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!doctype html>
+<html lang="en">
+  <head>
   <link rel="icon" type="image/x-icon" href="./assets/images/fav.png">
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="./assets/css/header.css">
+    <!-- Required meta tags -->
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ <link rel="stylesheet" href="./assets/css/header.css">
   <link rel="stylesheet" href="./assets/css/contact.css">
   <link rel="stylesheet" href="./assets/css/courses.css">
   <link rel="stylesheet" href="./assets/css/style.css">
@@ -14,71 +14,48 @@
   <link rel="stylesheet" href="./assets/css/login.css">
   <link rel="stylesheet" href="./assets/css/single-course.css">
   <link rel="stylesheet" href="./assets/css/footer.css">
-  
-
-  
- 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-  
-
-
-</head>
-<body>
-
- 
-<!----------navbar-->
-<section id="navbar">
-<div class="container-fluid">
-<nav class="navbar navbar-expand-lg">
-  <a class="navbar-brand" href="index.php"><img src="./assets/images/main-logo.png" alt="logo" class="img-fluid"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      
-
-    <li class="nav-item">
-        <a class="nav-link" href="index.php">Home</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="courses.php">Courses</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="schedual.php">Upcoming Events </a>
-      </li>
-
-      
-      <li class="nav-item">
-          <a class="nav-link" href="contact.php">Contact Us</a>
-        </li>
-
-
-
-        <li class="nav-item sign-in-li">
-          <?php
-            if(isset($_SESSION['loggedin'])){
-          ?>
-          <a class="nav-link" href="ManageCourse.php">Go to Backoffice</a>
-          <?php }else{ ?>
-            <a class="nav-link" href="login.php">Sign In</a>
-            <?php } ?>
-        </li>
-      
-      
-        
-        
-      
-
-        
-        
-    </ul>
-    
-  </div>
-</nav>
-</div>
-</section>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  </head>
+  <body>
+    <!-----navbar------->
+    <div class="container-fluid navbar-bg">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="navbar-main">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <a class="navbar-brand" href="index.php"><img src="./assets/images/main-logo.png" alt="QamsoftLogo" class="img-fluid"></a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="navbar-toggler-icon"></span>
+                        </button>
+                      
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                          <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                              <a class="nav-link" href="courses.php">Courses <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="schedual.php">Upcoming Events</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact.php">Contact</a>
+                              </li>
+                              <li class="nav-item sign-in">
+                                <a class="nav-link" href="login.php">Sign In</a>
+                              </li>
+                          </ul>
+                          
+                        </div>
+                      </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-------navbar-->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
